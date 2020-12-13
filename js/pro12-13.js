31ee4a2ce00bf3c3ed101b4aed15e154737d56b5
tomxin <2199215504@qq.com>
2020-12-13 16:42:38 +0800

    js-first

---
+++ js/pro12-13.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-13 11:19:46
+ * @LastEditTime: 2020-12-13 15:31:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \JS\js\pro12-13.js
+ */
+    var oBox =document.getElementById('box');
+    oBox.onclick =function(){
+        alert('登录');
+    }
