33430ccdd0ad337a75ae57fc03d6576229496322
tomxin <2199215504@qq.com>
2021-03-11 21:05:56 +0800

    reg

--- js/pro12-13.js
+++ js/pro12-13.js
@@ -1,12 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-12-13 11:19:46
- * @LastEditTime: 2020-12-13 15:31:58
+ * @LastEditTime: 2020-12-24 18:45:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \JS\js\pro12-13.js
  */
-    var oBox =document.getElementById('box');
-    oBox.onclick =function(){
-        alert('登录');
-    }
